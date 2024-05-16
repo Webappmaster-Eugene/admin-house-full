2eee6aea000ceeba1c66a5c61eedde31ae171993
Webappmaster-Eugene <johnn.hotmail@mail.ru>
2024-05-16 10:29:32 +0300

    fix: refactoring field-of-material

--- back/src/common/generated/zod/index.ts
+++ back/src/common/generated/zod/index.ts
@@ -1,5 +1,5 @@
 import { z } from 'zod';
-import type { Prisma } from '../../../../node_modules/.prisma/client';
+import type { Prisma } from '../../../../node_modules/@prisma/client';
 
 /////////////////////////////////////////
 // HELPER FUNCTIONS
