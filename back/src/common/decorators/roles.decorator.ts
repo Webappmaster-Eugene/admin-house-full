bad0c958e41fb82a80a07c55e79efa4680c5e222
Webappmaster-Eugene <johnn.hotmail@mail.ru>
2024-05-18 07:09:05 +0300

    patch logic errors

--- back/src/common/decorators/roles.decorator.ts
+++ back/src/common/decorators/roles.decorator.ts
@@ -1,5 +1,5 @@
 import { SetMetadata } from '@nestjs/common';
-import { EUserTypeVariants } from '.prisma/client';
+import type { EUserTypeVariants } from '.prisma/client';
 
 export const RolesSetting = (...roles: EUserTypeVariants[]) => {
   return SetMetadata('roles', roles);
